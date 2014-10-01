0f004a2f4080ee9deedadb1453f2a58471b741a8
happyginger <winter_evening@163.com>
2014-10-01 20:52:30 +0800

    ver0.3 change mode switch to char

--- C#/DataChange/DataChange/Program.cs
+++ C#/DataChange/DataChange/Program.cs
@@ -15,10 +15,10 @@ namespace DataChange
             do
             {
                 Console.WriteLine("模式选择\n1.将字符串转换为Base64编码\n2.将Base64编码转换成字符串\n3.转换一个十进制数\n4.转换一个二进制数\n5.转换一个十六进制数");
-                int mode = int.Parse(Console.ReadLine());
+                char mode =char.Parse(Console.ReadLine());
                 switch (mode)
                 {
-                    case 1: Console.WriteLine("请输入一个字符串：");
+                    case '1': Console.WriteLine("请输入一个字符串：");
                         string strings = Console.ReadLine();
                         byte[] bytestrings = Encoding.Default.GetBytes(strings);
                         string base64 = Convert.ToBase64String(bytestrings);
@@ -26,7 +26,7 @@ namespace DataChange
                         Console.WriteLine("是否继续？（y or n)");
                         next = char.Parse(Console.ReadLine());
                         break;
-                    case 2: Console.WriteLine("请输入一个Base64编码：");
+                    case '2': Console.WriteLine("请输入一个Base64编码：");
                         string base64_2 = Console.ReadLine();
                         byte[] base64_byte = Convert.FromBase64String(base64_2);
                         string strings2 = Encoding.Default.GetString(base64_byte);
@@ -34,19 +34,19 @@ namespace DataChange
                         Console.WriteLine("是否继续？（y or n)");
                         next = char.Parse(Console.ReadLine());
                         break;
-                    case 3: Console.WriteLine("请输入一个十进制数：");
+                    case '3': Console.WriteLine("请输入一个十进制数：");
                         int num10 = int.Parse(Console.ReadLine());
                         Console.WriteLine("十进制{0}的二进制为{1}\n十六进制为0x{2}", num10, Convert.ToString(num10, 2),Convert.ToString(num10 ,16));
                         Console.WriteLine("是否继续？（y or n)");
                         next = char.Parse(Console.ReadLine());
                         break;
-                    case 4: Console.WriteLine("请输入一个二进制数：");
+                    case '4': Console.WriteLine("请输入一个二进制数：");
                         string num2 = Console.ReadLine();
                         Console.WriteLine("二进制{0}b的十进制为{1}\n十六进制为0x{2}", num2, Convert.ToInt32(num2,2),Convert .ToString (Convert.ToInt32 (num2 ,2),16));
                         Console.WriteLine("是否继续？（y or n)");
                         next = char.Parse(Console.ReadLine());
                         break;
-                    case 5:Console.WriteLine("请输入一个十六进制数：");
+                    case '5':Console.WriteLine("请输入一个十六进制数：");
                         string num16 = Console.ReadLine();
                         Console.WriteLine("十六进制0x{0}的十进制为{1}\n二进制为{2}b", num16 , Convert.ToInt32(num16 ,16),Convert .ToString (Convert.ToInt32 (num16 ,16),2));
                         Console.WriteLine("是否继续？（y or n)");
@@ -58,7 +58,7 @@ namespace DataChange
                         break;
                 }
 
-            } while (next == 'y');
+            } while (next == 'y'|next =='Y');
 
         }
     }
